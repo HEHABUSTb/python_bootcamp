c73741324da85a479e35d079770bd7289d7d2a85
Aleksandr Liubinskii <a.liubinskii@hexacore.io>
2023-07-17 17:47:11 +0400

    Day_1 Band name generator

---
+++ Day_1-10/Day_1_Band_Generator.py
@@ -0,0 +1,11 @@
+def band_generator():
+    print("Welcome to the Band Name Generator")
+
+    city = input("What's name of the city you grew up in?\n")
+    pet_name = input("What's your pet's name?\n")
+
+    print(f"Your band name could be {city} {pet_name}")
+
+
+if __name__ == '__main__':
+    band_generator()

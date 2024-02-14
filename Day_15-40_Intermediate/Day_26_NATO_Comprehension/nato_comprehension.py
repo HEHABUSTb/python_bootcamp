4bc454527e48e3585d82c62396a2c2e70b98bed7
HEHABUSTb <msanvold@gmail.com>
2024-02-14 15:46:53 +0100

    Day_30 add try except block for nato_comprehension.py

--- Day_15-40_Intermediate/Day_26_NATO_Comprehension/nato_comprehension.py
+++ Day_15-40_Intermediate/Day_26_NATO_Comprehension/nato_comprehension.py
@@ -15,9 +15,18 @@ for (index, row) in data.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 # TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(nato_alphabet)
+print(nato_alphabet)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input(f"Enter your word: ")
-result = [nato_alphabet[letter] for letter in user_input.upper()]
-print(result)
+
+def nato():
+    try:
+        # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+        user_input = input(f"Enter your word: ")
+        result = [nato_alphabet[letter] for letter in user_input.upper()]
+        print(result)
+    except KeyError as e:
+        print('Sorry only letters please')
+        nato()
+
+
+nato()

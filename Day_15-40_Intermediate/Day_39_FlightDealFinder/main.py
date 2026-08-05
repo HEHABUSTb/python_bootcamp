007a6baab1942f8c921eb4e9103640bf4744e9fe
a.lubinskii <a.lubinskii@strikerz.inc>
2026-08-05 16:49:08 +0200

    Add entry for messages;

--- Day_15-40_Intermediate/Day_39_FlightDealFinder/main.py
+++ Day_15-40_Intermediate/Day_39_FlightDealFinder/main.py
@@ -11,11 +11,17 @@ flight_search = FlightSearch()
 city_data = manager.get_data_from_sheet()
 
 outbound_date = (datetime.now() + timedelta(weeks=20)).strftime("%Y-%m-%d")
-data = []
+full_data = []
+data_for_message = []
+
 for city in city_data:
     flights  = flight_search.get_best_flight(arrival_id=city['iataCode'])
     best_three = sorted(flights, key=lambda flight: int(flight.price))[:3]
-    data.append(best_three)
+    full_data.append(best_three)
+    for best in best_three:
+        if int(best.price) < int(city['lowestPrice']):
+            data_for_message.append({city['city']: best})
 
-pprint(data)
+print(data_for_message)
+# pprint(full_data)
 

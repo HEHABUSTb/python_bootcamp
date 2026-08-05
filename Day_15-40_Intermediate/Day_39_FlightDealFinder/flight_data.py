0bfb7651af1ca51549f26686d85b4272de13d93a
a.lubinskii <a.lubinskii@strikerz.inc>
2026-08-05 16:17:26 +0200

    add flight search api with data class

--- Day_15-40_Intermediate/Day_39_FlightDealFinder/flight_data.py
+++ Day_15-40_Intermediate/Day_39_FlightDealFinder/flight_data.py
@@ -1,3 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    price: str
+    departure_airport: str
+    arrival_airport: str
+    duration: str
+    booking_token: str
+
+    @classmethod
+    def from_json(cls, flight: dict, departure_id: str, arrival_id: str) -> 'FlightData':
+        return cls(
+            price=flight["price"],
+            departure_airport=departure_id,
+            arrival_airport=arrival_id,
+            duration=flight["total_duration"],
+            booking_token=flight["booking_token"],
+        )
--- Day_15-40_Intermediate/Day_39_FlightDealFinder/flight_search.py
+++ Day_15-40_Intermediate/Day_39_FlightDealFinder/flight_search.py
@@ -1,3 +1,48 @@
+from datetime import datetime, timedelta
+from pprint import pprint
+from flight_data import FlightData
+
+from base_data_class import BaseDataClass
+from env import SERP_API
+import serpapi
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.url = r"https://serpapi.com/search.json?engine=google_flights"
+        self.departure_id = r"WAW"
+        self.client = serpapi.Client(api_key=SERP_API)
+
+    @property
+    def get_outbound_date(self) -> str:
+        outbound_date = (datetime.now() + timedelta(weeks=1)).strftime("%Y-%m-%d")
+        return outbound_date
+
+
+    def get_best_flight(self, arrival_id: str = "CDG", outbound_date: str = None) -> list[FlightData]:
+        if not outbound_date:
+            outbound_date = self.get_outbound_date
+
+        results = self.client.search({
+            "engine": "google_flights",
+            "departure_id": self.departure_id,
+            "arrival_id": arrival_id,
+            "currency": "PLN",
+            "type": "2",
+            "outbound_date": outbound_date
+        })
+
+        best_flights = results["best_flights"]
+        # pprint(best_flights)
+
+        result = [FlightData.from_json(flight=flight, departure_id=self.departure_id, arrival_id=arrival_id) for flight in best_flights]
+        pprint(result)
+
+        return result
+
+
+
+if __name__ == "__main__":
+    manager = FlightSearch()
+    manager.get_best_flight()
+

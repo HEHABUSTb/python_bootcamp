d4c39323304653b436e583e322e47a8841cfd394
a.lubinskii <a.lubinskii@strikerz.inc>
2026-08-05 14:43:01 +0200

    pip 8

--- Day_15-40_Intermediate/Day_39_FlightDealFinder/data_manager.py
+++ Day_15-40_Intermediate/Day_39_FlightDealFinder/data_manager.py
@@ -1,20 +1,19 @@
 from base_data_class import BaseDataClass
 from env import SHEETY_KEY
 
+
 class DataManager(BaseDataClass):
 
     def __init__(self):
         self.url = f"https://api.sheety.co/{SHEETY_KEY}/flightFinder/лист1"
 
-
     @property
     def get_url(self) -> str:
         return self.url
 
-
     def get_data_from_sheet(self):
-        result = self._get(self.url, {}, {})
 
+        result = self._get(self.url, {}, {})
         self.classify_response(result.status_code)
 
         return result.json().get("лист1")

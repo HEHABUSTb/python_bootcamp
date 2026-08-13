97cac9f20cbc5f0d1e5628a44780a2c6f35b6d5b
a.lubinskii <a.lubinskii@strikerz.inc>
2026-08-13 10:29:34 +0200

    Remove annoying to do comments

--- Day_15-44_Intermediate/Day_26_NATO_Comprehension/nato_comprehension.py
+++ Day_15-44_Intermediate/Day_26_NATO_Comprehension/nato_comprehension.py
@@ -13,14 +13,14 @@ for (index, row) in data.iterrows():
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
-# TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+#  1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 print(nato_alphabet)
 
 
 def nato():
     try:
-        # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+        # 2. Create a list of the phonetic code words from a word that the user inputs.
         user_input = input(f"Enter your word: ")
         result = [nato_alphabet[letter] for letter in user_input.upper()]
         print(result)

d18f212f32fd907a292d7e73d9cf1c99341f0110
a.lubinskii <a.lubinskii@strikerz.inc>
2026-08-26 09:03:10 +0200

    Day 48 refactoring

---
+++ Day_45-59_Intermediate+/Day_48_Selenium/cookie_clicker_refactoring.py
@@ -0,0 +1,91 @@
+from codecs import ignore_errors
+from dataclasses import dataclass
+from pprint import pprint
+
+from selenium import webdriver
+from selenium.common import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from types import SimpleNamespace
+from datetime import datetime, timedelta
+
+from selenium.webdriver.common.devtools.v85.network import Cookie
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Selectors
+LANGUAGE_BUTTON = (By.ID, "langSelect-EN")
+COOKIE_BUTTON = (By.ID, "bigCookie")
+COOKIE_NUMBER = (By.ID, "cookies")
+
+# Upgrades selectors
+UNLOCKED_PRODUCTS = (By.CSS_SELECTOR, ".product.unlocked.enabled")
+PRICE = (By.CSS_SELECTOR, ".price")
+UNLOCKED_UPGRADES = (By.CSS_SELECTOR, ".crate.upgrade.enabled")
+
+# Base url
+BASE_URL = r"https://ozh.github.io/cookieclicker/"
+
+
+
+
+class CookieClicker:
+
+    def __init__(self, ):
+        self.driver = webdriver.Chrome()
+        self.timelimit = 1
+
+    def main(self):
+        self.open_browser()
+        self.select_language()
+        self.click_cookie()
+
+    def click_cookie(self):
+        # Start actually play
+        timelimit = 1  # mins
+        end_time = datetime.now() + timedelta(minutes=timelimit)
+        next_product_check = datetime.now()
+
+        while datetime.now() < end_time:
+
+            # check for upgrades every 5 sec
+            if datetime.now() >= next_product_check:
+                best_product = self.find_best_product()
+                if best_product:
+                    best_product.click()
+                next_product_check = datetime.now() + timedelta(seconds=5)
+
+            # just click cookie
+            self.driver.find_element(*COOKIE_BUTTON).click()
+
+        print(f"Cookies per second:'{self.driver.find_element(*COOKIE_NUMBER).text}'")
+
+    def open_browser(self):
+        self.driver.get(BASE_URL)
+
+    def select_language(self):
+        language_button = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable(LANGUAGE_BUTTON))
+        language_button.click()
+
+    def find_best_product(self) -> WebElement | None:
+        products = self.driver.find_elements(*UNLOCKED_PRODUCTS)
+        if not products:
+            print("No products found")
+            return None
+
+        best_price = 0
+        best_index = 0
+
+        for i in range(len(products)):
+            price = products[i].find_element(*PRICE)
+            if best_price < int(price.text):
+                best_price = int(price.text)
+                best_index = i
+
+        return products[best_index]
+
+if __name__ == "__main__":
+    cookie_clicker = CookieClicker()
+    cookie_clicker.main()
+
